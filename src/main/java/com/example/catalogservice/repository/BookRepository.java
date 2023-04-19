9ac3616bd94aa6ec476b949caa4d27a1180abe1f
Laurentiu_Zamfirache <laurentiu.zamfirache@yahoo.com>
2023-04-19 11:19:43 +0300

    first commit

---
+++ src/main/java/com/example/catalogservice/repository/BookRepository.java
@@ -0,0 +1,14 @@
+package com.example.catalogservice.repository;
+
+import com.example.catalogservice.domain.Book;
+
+import java.util.Optional;
+
+public interface BookRepository {
+
+    Iterable<Book> findAll();
+    Optional<Book> findByIsbn(String isbn);
+    boolean existsByIsbn(String isbn);
+    Book save(Book book);
+    void deleteByIsbn(String isbn);
+}
